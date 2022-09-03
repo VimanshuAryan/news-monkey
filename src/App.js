390cbf7ca687a3afcd1d80daf98562a3770f6341
Vimanshu Aryan <vimanshuaryan@gmail.com>
2022-09-04 01:18:11 +0530

    Adding functionalities and darkmode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React,{useState} from 'react';
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+
+  const modeHandler = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#120236';
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  };
+
   return (
     <>
-    <Navbar/>
+    <Navbar mode={mode} toggleMode={modeHandler}/>
     <div className="container my-3">
-      <TextForm heading = "Enter the text to analyze."/>
+    <TextForm heading = "Enter the text to analyze." mode = {mode}/>
+    {/* <About/> */}
     </div>
     
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 function TextForm(props) {
 
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
 
     const upperCaseHandler = (event) => {
         setText(text.toLocaleUpperCase());
@@ -16,29 +16,46 @@ function TextForm(props) {
         setText(event.target.value);
     };
 
+    const copyTextHandler = () => {
+        let text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    };
+
+    const extraSpacesHandler = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    };
+
     return (
         <>
-        <div className="container">
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-            <textarea
-                className="form-control"
-                id="exampleFormControlTextarea1"
-                rows="8"
-                onChange={onChangeHandler}
-                value={text}>
-            </textarea>
+            <div className="container" style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea
+                        style={{
+                            backgroundColor: props.mode === 'light' ? 'white' : 'grey',
+                            color: props.mode === 'light' ? 'black' : 'white'
+                        }}
+                        className="form-control"
+                        id="myBox"
+                        rows="8"
+                        onChange={onChangeHandler}
+                        value={text}>
+                    </textarea>
+                </div>
+                <button className="btn btn-primary mx-3" onClick={upperCaseHandler}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-3" onClick={lowerCaseHandler}>Convert to Lowercase</button>
+                <button className="btn btn-primary mx-3" onClick={copyTextHandler}>Copy Text</button>
+                <button className="btn btn-primary mx-3" onClick={extraSpacesHandler}>Remove extra spaces</button>
             </div>
-            <button className="btn btn-primary mx-3" onClick={upperCaseHandler}>Convert to Uppercase</button>
-            <button className="btn btn-primary mx-3" onClick={lowerCaseHandler}>Convert to Lowercase</button>
-        </div>
-        <div className="container my-3">
-            <h1>Your Text Summary</h1>
-            <p>{text.split(" ").length} words and {text.length} characters</p>
-            <p>{0.008 * text.split(" ").length} minutes read</p>
-            <h2>Preview</h2>
-            <p>{text}</p>
-        </div>
+            <div className="container my-3" style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
+                <h2>Your Text Summary</h2>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} minutes read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
         </>
     )
 }
