e1f484c9afa2f8cdd96914e0029a5b0aa27be938
Vimanshu Aryan <vimanshuaryan@gmail.com>
2022-09-05 17:40:38 +0530

    Alert functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,10 +6,12 @@ function TextForm(props) {
 
     const upperCaseHandler = (event) => {
         setText(text.toLocaleUpperCase());
+        props.showAlert("Converted to Uppercase !", "success");
     };
 
     const lowerCaseHandler = (event) => {
         setText(text.toLocaleLowerCase());
+        props.showAlert("Converted to Lowercase !", "success");
     };
 
     const onChangeHandler = (event) => {
@@ -20,11 +22,13 @@ function TextForm(props) {
         let text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        props.showAlert("Text copied to clipboard !","success");
     };
 
     const extraSpacesHandler = () => {
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
+        props.showAlert("Extra spaces removed !", "success");
     };
 
     return (
